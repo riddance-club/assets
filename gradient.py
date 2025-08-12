1deb9dbff2e8d1374df71b4b7f2a05b1885505c6
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2025-08-12 14:49:53 +0300

    Update gradient.py

--- gradient.py
+++ gradient.py
@@ -1,12 +1,7 @@
-from PIL import Image
-import numpy as np
-def g(w,h):
- s=[(0,(154,242,248)),(0.18,(214,154,203)),(0.38,(141,119,197)),(0.725,(91,111,183)),(0.99,(120,200,255)),(1,(154,242,248))]
- t=((np.linspace(0,1,w)[None,:]+np.linspace(0,1,h)[:,None])/2)%1
- a=np.zeros((h,w,3),float)
- for(p0,c0),(p1,c1) in zip(s,s[1:]):
-  m=(t>=p0)&(t<=p1)
-  l=(t[m]-p0)/(p1-p0)
-  a[m]=(1-l)[:,None]*c0+l[:,None]*c1
- return Image.fromarray(a.clip(0,255).astype('uint8'))
-g(580,440).save('gradient.png')
+from PIL import Image as F,ImageFilter as M,ImageEnhance as G
+import numpy as C
+def A(w,h):
+	B=None;H=[(0,(120,200,255)),(.18,(214,154,203)),(.38,(141,119,197)),(.5,(91,111,183)),(.75,(120,200,255)),(1,(154,242,248))];D=(C.linspace(0,1,w)[B]+C.linspace(0,1,h)[:,B])/2%1;I=C.zeros((h,w,3),float)
+	for((E,N),(J,O))in zip(H,H[1:]):K=(D>=E)&(D<=J);L=(D[K]-E)/(J-E);I[K]=N*(1-L)[:,B]+O*L[:,B]
+	A=F.fromarray(I.clip(0,255).astype('uint8'));P=G.Brightness(A.filter(M.GaussianBlur(12))).enhance(1.6);A=F.blend(A,P,.5);return G.Color(A).enhance(1.3)
+A(580,440).save('gradient.png')
